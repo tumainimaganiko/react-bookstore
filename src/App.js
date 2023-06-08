9234ae9135ab5b26aa0d04a882589392fc55d054
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-09 00:21:38 +0300

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Books from 'components/Books';
+import Categories from 'components/Categories';
 import './App.css';
 
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
+}
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Books />}/>
+        <Route path="categories" element={<Categories />} />
+      </Route>
+    </Routes>
   );
 }
 
