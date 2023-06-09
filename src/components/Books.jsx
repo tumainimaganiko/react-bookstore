89806ef94096787e12376da2195d117c95581a41
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-09 20:38:54 +0300

    Use map method to pass props

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,38 @@
 import BookState from 'components/BookState';
 import AddBook from 'components/AddBook';
 
-const Books = () => (
-  <div>
-    <BookState category="Action" title="The Hunger Games" author="Suzanne Collins" percentage="98" />
-    <BookState category="Science Fiction" title="Dune" author="Frank Herbert" percentage="32" />
-    <BookState category="Economy" title="Capitalism" author="Suzanne Collins" percentage="12" />
-    <AddBook />
-  </div>
-);
+const Books = () => {
+  const data = [
+    {
+      id: 1,
+      category: 'Action',
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      percentage: '98',
+    },
+    {
+      id: 2,
+      category: 'Science Fiction',
+      title: 'Dune',
+      author: 'Frank Herbert',
+      percentage: '32',
+    },
+    {
+      id: 3,
+      category: 'Economy',
+      title: 'Capitalism',
+      author: 'Suzanne Collins',
+      percentage: '12',
+    },
+  ];
 
+  return (
+    <div>
+      {data.map((item) => (
+        <BookState key={item.id} item={item} />
+      ))}
+      <AddBook />
+    </div>
+  );
+};
 export default Books;
