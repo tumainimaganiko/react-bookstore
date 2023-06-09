366f94572faae9e53065e50a975523bccc3f7339
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-09 09:43:10 +0300

    Add CSS styles to Components

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom";
-
+import styles from 'styles/BookDetails.module.css'
 const BookState = () => {
     return (
-       <div>
-         <div>
-            <h3>Action</h3>
-            <h2>The Hunger Games</h2>
-            <h2>Suzanne Collins</h2>
+       <div className={styles.container}>
+         <div className={styles.details}>
+            <h4>Action</h4>
+            <h3>The Hunger Games</h3>
+            <p>Suzanne Collins</p>
             <ul>
                 <li><Link to="comments">Comments</Link></li>
                 <li><Link to="remove">Remove</Link></li>
                 <li><Link to="edit">Edit</Link></li>
             </ul>
         </div>
-        <div>
+        <div className={styles.progress}>
             <div>
                 <h2>64%</h2>
                 <p>completed</p>
             </div>
-            <div>
+            <div className={styles.updateProgress}>
                 <h3>CURRENT CHAPTER</h3>
-                <h3>Chapetr 17</h3>
+                <h3>Chapter 17</h3>
                 <button>UPDATE PROGRESS</button>
             </div>
         </div>
