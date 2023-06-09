f4d4bf6b71bb195b51279936700946b2c7f7ba29
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-09 12:04:08 +0300

    Add User Icon and CSS styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import { NavLink } from "react-router-dom";
+import { FaUserAlt } from "react-icons/fa";
 import styles from 'styles/Navbar.module.css'
 
 const Navbar = () => {
+    const display = {
+        display: 'flex',
+        'align-items': 'center'
+    }
     return (
         <div className={styles.container}>
+            <div style={display}>
             <h1>Bookstore CMS</h1>
             <ul>
                 <li><NavLink to="/" className={styles.link}>Books</NavLink></li>
                 <li><NavLink to="categories" className={styles.link}>Categories</NavLink></li>
             </ul>
+            </div>
+            <div className={styles.icon}><FaUserAlt className={styles.color} /></div>
         </div>
     )
 }
