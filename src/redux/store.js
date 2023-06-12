05154bd8609ece0d6e6da5a3efddf6532865aa33
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-12 22:51:54 +0300

    Fix Bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
-import booksReducer from "./books/booksSlice";
-import categoriesReducer from "./categories/categoriesSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   books: booksReducer,
