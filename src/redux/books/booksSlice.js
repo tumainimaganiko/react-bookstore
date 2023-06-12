9b7dce887649fa3fac6ee64bb1dfb6d8d0df0101
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-12 22:40:50 +0300

    Format code

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,11 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialstate = {
   books: [],
 };
 
 export const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialstate,
   reducers: {
     addBook: (state, action) => {
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,15 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialstate = {
   categories: [],
 };
 
 export const categoriesSlice = createSlice({
-  name: 'categories',
+  name: "categories",
   initialstate,
   reducers: {
     checkStatus: (state, action) => {
-      state.categories = action.payload === 'Under construction' ? 'Under Construction' : state.categories;
+      state.categories =
+        action.payload === "Under construction"
+          ? "Under Construction"
+          : state.categories;
     },
   },
 });
