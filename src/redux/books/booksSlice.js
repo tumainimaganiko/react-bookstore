af91a971971701e1484558ca819e3b8befbe288b
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-13 07:03:50 +0300

    Create intialState for booksSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialstate = {
-  books: [],
+  books: // Initial state:
+  [
+    {
+      "item_id": "item1",
+      "title": "The Great Gatsby",
+      "author": "John Smith",
+      "category": "Fiction"
+    },
+    {
+      "item_id": "item2",
+      "title": "Anna Karenina",
+      "author": "Leo Tolstoy",
+      "category": "Fiction"
+    },
+    {
+      "item_id": "item3",
+      "title": "The Selfish Gene",
+      "author": "Richard Dawkins",
+      "category": "Nonfiction"
+    }
+  ]
 };
 
 export const booksSlice = createSlice({
