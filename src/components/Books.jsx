4d5461524c161c9db2806c927243397cbc6f55f0
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-13 07:10:43 +0300

    Import Data by useSelector hook

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,35 +1,37 @@
+import { useSelector } from 'react-redux';
 import BookState from 'components/BookState';
 import AddBook from 'components/AddBook';
 
 const Books = () => {
-  const data = [
-    {
-      id: 1,
-      category: 'Action',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      percentage: '98',
-    },
-    {
-      id: 2,
-      category: 'Science Fiction',
-      title: 'Dune',
-      author: 'Frank Herbert',
-      percentage: '32',
-    },
-    {
-      id: 3,
-      category: 'Economy',
-      title: 'Capitalism',
-      author: 'Suzanne Collins',
-      percentage: '12',
-    },
-  ];
+  const { books } = useSelector(store => store.books)
+  // const data = [
+  //   {
+  //     id: 1,
+  //     category: 'Action',
+  //     title: 'The Hunger Games',
+  //     author: 'Suzanne Collins',
+  //     percentage: '98',
+  //   },
+  //   {
+  //     id: 2,
+  //     category: 'Science Fiction',
+  //     title: 'Dune',
+  //     author: 'Frank Herbert',
+  //     percentage: '32',
+  //   },
+  //   {
+  //     id: 3,
+  //     category: 'Economy',
+  //     title: 'Capitalism',
+  //     author: 'Suzanne Collins',
+  //     percentage: '12',
+  //   },
+  // ];
 
   return (
     <div>
-      {data.map((item) => (
-        <BookState key={item.id} item={item} />
+      {books.map((item) => (
+        <BookState key={item.item_id} item={item} />
       ))}
       <AddBook />
     </div>
